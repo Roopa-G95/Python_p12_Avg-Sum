c48561fec6d68c550ca41916159779fe0f349125
Roopa-G95 <roopagokulakrishnan95@gmail.com>
2024-12-03 11:27:07 +0530

    Marks

---
+++ Average_sum.py
@@ -0,0 +1,6 @@
+a,b,c,d,e=input('Enter 5 subject marks with comma').split(",")
+
+Total = int(a)+int(b)+int(c)+int(d)+int(e)
+Average = int(Total/5)
+print('Total Marks =', Total)
+print('Average Marks=',Average)
